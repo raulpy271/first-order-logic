2539728ec57a4ad1da2ba05256b3ca7558f3e05b
Raul <raulrufino0@gmail.com>
2022-07-28 08:54:03 -0300

    Add input to insert formula

--- src/gui/App.js
+++ src/gui/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputAndOutputDiv">
+        <div className="inputDiv">
+          <label htmlFor="formulaInput">Insert the formula:</label><br/>
+          <textarea id="formulaInput" name="formulaInput" rows="4" cols="50">
+            *x0 (P(x0) | (~ P(x0))) 
+          </textarea>
+        </div>
+      </div>
     </div>
   );
 }
