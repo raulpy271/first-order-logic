ac51e0b6ac2426b43764b74d4e9b840d458d74dc
Raul <raulrufino0@gmail.com>
2022-07-29 08:33:38 -0300

    Add some formula examples

--- src/gui/App.js
+++ src/gui/App.js
@@ -42,8 +42,28 @@ class App extends React.Component {
             <textarea id="formulaInput" name="formulaInput" rows="4" cols="50" defaultValue="*x0 (P(x0) | (~ P(x0)))">
             </textarea><br/>
             <input type="submit" value="Check formula" onClick={() => this.checkFormulaBtn()}></input><br/>
-            <p id="formulaOutput"></p>
           </div>
+          <p id="formulaOutput">Parse result: <i>waiting input...</i></p>
+        </div>
+        <div className="formulaExamples">
+          See some examples:
+          <ul>
+            <li>
+              Using disjunction: <span className="formulaSyntax"> (y1 | y2) </span>
+            </li>
+            <li>
+              Using conjunction: <span className="formulaSyntax"> (y1 & y2) </span>
+            </li>
+            <li>
+              Using implication: <span className="formulaSyntax"> (y1 =&gt; P(x12)) </span>
+            </li>
+            <li>
+              Exists a x0 that don't have a property: <span className="formulaSyntax">!x0 (~ P(x0)) </span>
+            </li>
+            <li>
+              For all x0, x0 implies x0: <span className="formulaSyntax">*x0 ( P(x0) =&gt; P(x0) ) </span>
+            </li>
+          </ul>
         </div>
       </div>
     );
