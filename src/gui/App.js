ff815c8546590420d54a0e70ca76c48ff5c781e9
Raul <raulrufino0@gmail.com>
2022-07-30 11:25:22 -0300

    Add option to specify list of predicate symbols used in formulas

--- src/gui/App.js
+++ src/gui/App.js
@@ -2,11 +2,48 @@ import React from 'react';
 
 import './App.css';
 import {formula_parser} from '../language-parser/formula.js';
+import {allPredicateSymbolsInTreeAreInThisArray, getAllPredicateSymbols, getPredicateSymbolsNotInExpectedArray} from '../language-parser/tree_analyzer.js';
+
+const empty_line_re = /^\s*$/;
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.use_custom_predicate_symbols = false;
+    this.array_of_predicate_symbols = [];
+  }
+
+  parseArrayOfCustomSymbols(text) {
+    let symbols = []
+    let lines = text.split('\n');
+    for (let line of lines) {
+      let valuesInLineWithOptionalWhitespace = line.split(',')
+      let valuesInLineWithEmptySpaces = valuesInLineWithOptionalWhitespace.map(v => v.trim());
+      let valuesInLine = valuesInLineWithEmptySpaces.filter(value => ! value.match(empty_line_re))
+      symbols = symbols.concat(valuesInLine);
+    }
+    return symbols;
+  }
+
+  setArrayOfCustomSymbols() {
+    let textCustomSymbols = document.getElementById('customPredicateSymbolsInput').value;
+    this.array_of_predicate_symbols = this.parseArrayOfCustomSymbols(textCustomSymbols);
+    console.log(`Setting array of custom predicates: ${JSON.stringify(this.array_of_predicate_symbols)}`);
+  }
+
+  selectPredicateSymbols() {
+    let predicateSymbolsCustomTag = document.getElementById('predicateSymbolsCustom');
+    let textarea = document.getElementById('customPredicateSymbolsInput');
+    this.use_custom_predicate_symbols = predicateSymbolsCustomTag.checked;
+    if (this.use_custom_predicate_symbols) {
+      textarea.style.display = "inline";
+    } else {
+      this.array_of_predicate_symbols = [];
+      textarea.style.display = "none";
+    }
+  }
 
   checkFormulaBtn() {
-    let empty_line_re = /^\s*$/;
     let text_content = document.getElementById("formulaInput").value;
     console.log(text_content);
     let lines_not_empty = text_content.split('\n').filter(line => ! line.match(empty_line_re))
@@ -20,7 +57,19 @@ class App extends React.Component {
         if (result.index < formula.length) {
           this.setOutput(`ParseError (position ${result.index}): got unexpected string "${formula.slice(result.index)}".`)
         } else {
-          this.setOutput('Success!');
+          if (!this.use_custom_predicate_symbols) {
+            this.setOutput('Success!');
+          } else {
+            this.setArrayOfCustomSymbols();
+            console.log(`all predicates in formula: ${getAllPredicateSymbols(result.result)}`)
+            let formulaUseCustomSymbols = allPredicateSymbolsInTreeAreInThisArray(result.result, this.array_of_predicate_symbols);
+            if (formulaUseCustomSymbols) {
+              this.setOutput('Success!');
+            } else {
+              this.setOutput(`Got unexpected predicate symbol "${getPredicateSymbolsNotInExpectedArray(result.result,  this.array_of_predicate_symbols)}".`)
+            }
+
+          }
         }
       }
     } else {
@@ -41,6 +90,14 @@ class App extends React.Component {
             <label htmlFor="formulaInput">Insert the formula:</label><br/>
             <textarea id="formulaInput" name="formulaInput" rows="4" cols="50" defaultValue="*x0 (P(x0) | (~ P(x0)))">
             </textarea><br/>
+            <div className="predicateSymbols">
+              <label id="predicateSymbolsPTag">Predicate symbols:</label><br/>
+              <input type="radio" id="predicateSymbolsAllSymbols" name="predicateSymbols" defaultChecked onChange={() => this.selectPredicateSymbols()}/>
+              <label htmlFor="predicateSymbolsAllSymbols">All visible characters</label>
+              <input type="radio" id="predicateSymbolsCustom" name="predicateSymbols" onChange={() => this.selectPredicateSymbols()}/>
+              <label htmlFor="predicateSymbolsCustom">A custom character set</label><br/>
+              <textarea id="customPredicateSymbolsInput" name="customPredicateSymbolsInput" rows="1" cols="30" defaultValue="P, Q, S"></textarea>
+            </div>
             <input type="submit" value="Check formula" onClick={() => this.checkFormulaBtn()}></input><br/>
           </div>
           <p id="formulaOutput">Parse result: <i>waiting input...</i></p>
